04703f2a4133a8718ea83cdb801fa06cb4b20746
Zakir <z.chalimov@yandex.ru>
2020-02-06 13:35:41 +0500

    Модель уведомления о статусе платежа

---
+++ models/paymentCallback.go
@@ -0,0 +1,53 @@
+package models
+
+import "encoding/json"
+
+// PaymentCallback уведомление о статусе платежа.
+type PaymentCallback struct {
+	// RegPayNum номер платежа.
+	RegPayNum string `json:"regPayNum"`
+
+	// State статус платежа
+	State string `json:"state"`
+
+	// TotalAmount сумма платежа в копейках.
+	TotalAmount string `json:"totalAmount"`
+
+	// CreatedDate время создания платежа (Время должно быть в формате: yyyy-mm-dd hh:mm:ss).
+	CreatedDate string `json:"createdDate"`
+
+	// ProviderServCode уникальный код провайдера услуг.
+	ProviderServCode string `json:"providerServCode"`
+
+	// ProviderName название провайдера услуг.
+	ProviderName string `json:"providerName"`
+
+	// ErrorCode код ошибки, используется для локализации проблемы.
+	ErrorCode string `json:"errorCode"`
+
+	// Error текст ошибки.
+	Error string `json:"error"`
+
+	// Message сообщение пользователю.
+	Message string `json:"message"`
+
+	// ProvisionServices флаг, была ли оказана услуга.
+	ProvisionServices string `json:"provisionServices"`
+
+	// ProcDate время проведения платежа в формате yyyy-mm-dd hh:mm:ss.
+	ProcDate string `json:"procDate"`
+
+	// Sign подпись.
+	Sign string `json:"sign"`
+}
+
+func NewPaymentCallback(paymentCallbackJson []byte) (PaymentCallback, error) {
+	paymentCallback := PaymentCallback{}
+
+	err := json.Unmarshal(paymentCallbackJson, &paymentCallback)
+	if err != nil {
+		return paymentCallback, err
+	}
+
+	return paymentCallback, nil
+}
